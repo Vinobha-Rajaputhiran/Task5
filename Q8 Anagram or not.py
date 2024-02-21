fafe7e0d871e320193d19ffa248f3cac6a8e92e7
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-02-21 21:02:16 +0530

    Update Q8 Anagram or not.py

--- Q8 Anagram or not.py
+++ Q8 Anagram or not.py
@@ -1,9 +1,9 @@
 input1= str(input("Enter first string: "))
-input2= str(input("Enter first string: "))
+input2= str(input("Enter second string: "))
 
 print("Are the two given strings Anagrams? ", end="")
 
-if (sorted(input1) == sorted(input2)):
+if (sorted(input1) == sorted(input2)): #counter() function can also be used
         print("True")
 else:
         print("False")
