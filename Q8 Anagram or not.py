66e51dab5e7f687ed31fce2ffc8469dca62493cc
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-02-28 15:57:07 +0530

    Update Q8 Anagram or not.py

--- Q8 Anagram or not.py
+++ Q8 Anagram or not.py
@@ -1,10 +1,12 @@
-input1= str(input("Enter first string: "))
-input2= str(input("Enter second string: "))
+#Write a program that takes a string and returns True if it is an anagram of another string, False otherwise.
+
+input1= str(input("Enter first string: ")) #Get string1 from user
+input2= str(input("Enter second string: ")) #Get string2 from user
 
 print("Are the two given strings Anagrams? ", end="")
 
-if (sorted(input1) == sorted(input2)): #counter() function can also be used
-        print("True")
+if (sorted(input1) == sorted(input2)): #counter() function can also be used here
+        print("True")  #If the sorted instance of string 1 is equal to that of string 2 then strings are a palindrome of each other.
 else:
-        print("False")
+        print("False")  #If the sorted instance of string 1 is not equal to that of string 2 then strings are not a palindrome of each other.
 
