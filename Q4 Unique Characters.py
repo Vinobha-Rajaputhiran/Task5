ca688bd56322fca9ad8a81610eb564533fc3e985
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-02-28 15:35:47 +0530

    Update Q4 Unique Characters.py

--- Q4 Unique Characters.py
+++ Q4 Unique Characters.py
@@ -1,4 +1,6 @@
-input= str(input("Enter a string: "))
-input2 = input.replace(' ', '') #removing space in given string
+#Write a program that tales a string and returns the number of unique characters in it.
+
+input= str(input("Enter a string: ")) #Get input value from user
+input2 = input.replace(' ', '') #removing space in the input string using the replace function
 x= set(input2) #converting the string to set data type as set does not accept duplicate values
-print('The number of unique character: ', len(x))
+print('The number of unique character: ', len(x)) #Printing the length of the set x which contains the unique characters of the input string.
