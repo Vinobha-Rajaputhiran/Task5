6c7ab7e46e64d87490681a701a928a98e86b9d77
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-02-28 15:38:38 +0530

    Update Q5 Palindrome.py

--- Q5 Palindrome.py
+++ Q5 Palindrome.py
@@ -1,7 +1,9 @@
-val1= input("Enter the string: ")
-val2= val1[::-1]
+#Write a program that takes a string and returns True if its a Palindrome and False, otherwise.
+
+val1= input("Enter the string: ") #Get input from user        
+val2= val1[::-1] #Reverse the string 
 print("Is the string palindrome?")
-if val1 == val2:
-    print("True")
+if val1 == val2: #IF statement to check if the reversed string is identical to the user input string.
+    print("True") #If the condition is satisfied, the user string is a Palindrome
 else:
-    print("False")
+    print("False") #If the condition is not satisfied, the user string is not a Palindrome
