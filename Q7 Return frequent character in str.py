fc45e9131483be3a5f6abef096677b974172a1c2
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:14:06 +0530

    Update Q7 Return frequent character in str.py

--- Q7 Return frequent character in str.py
+++ Q7 Return frequent character in str.py
@@ -1,10 +1,14 @@
 #Write a program that takes a string and returns the most frequent character in it.
 
-input= str(input("Enter your string: ")) #Get input string from user
-input1=list(input.replace(" ","")) #Utilized replace function to remove any spaces in the user input
+input= str(input("Enter your string: ")) 
+#Get input string from user
+input1=list(input.replace(" ","")) 
+#Utilized replace function to remove any spaces in the user input
 
 print("The most common character in the string is: ", end="")
 
-from collections import Counter #counter function is used to find the frequency of each character
+from collections import Counter 
+#counter function is used to find the frequency of each character
 ordered= Counter(input1)
-print(ordered.most_common(1)[0][0]) #The first aka highest value is taken
+print(ordered.most_common(1)[0][0]) 
+#The first aka highest value is taken
