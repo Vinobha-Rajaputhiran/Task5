14ecc73b361b2cce592c5e5401eeb00c59c32c2d
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:11:29 +0530

    Update Q3 Vowels removal.py

--- Q3 Vowels removal.py
+++ Q3 Vowels removal.py
@@ -1,6 +1,8 @@
 #Write a program that takes a string and returns a new string with all the vowels removed.
-
-input = input('Enter the statement: ') #Enter the user input statement
-for x in input: #FOR loop to iterate the input string 
-    if x!='a' and x!='e' and x!='i' and x!='o' and x!='u' and x!='A' and x!='E' and x!='I' and x!='O' and x!='U': #IF condition to print non-vowel strings
+#Enter the user input statement
+input = input('Enter the statement: ')
+for x in input: 
+    #FOR loop to iterate the input string 
+    if x!='a' and x!='e' and x!='i' and x!='o' and x!='u' and x!='A' and x!='E' and x!='I' and x!='O' and x!='U': 
+        #IF condition to print non-vowel strings
         print(x, end='')
