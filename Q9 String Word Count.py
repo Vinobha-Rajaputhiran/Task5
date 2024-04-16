ba63d3a51a99104597cc0ad61f266c6b55916e53
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:14:46 +0530

    Update Q9 String Word Count.py

--- Q9 String Word Count.py
+++ Q9 String Word Count.py
@@ -1,5 +1,8 @@
 #Write a program that takes a string and returns the number of words in it.
 
-input= input("Enter the string: ") #Get input from user
-words = input.split() #Using the split function break the string into a list of words at the presence of a space.
-print("The number of words in the string: ", len(words)) #Print the length of the list to get the number of words in the string.
+input= input("Enter the string: ") 
+#Get input from user
+words = input.split() 
+#Using the split function break the string into a list of words at the presence of a space.
+print("The number of words in the string: ", len(words)) 
+#Print the length of the list to get the number of words in the string.
