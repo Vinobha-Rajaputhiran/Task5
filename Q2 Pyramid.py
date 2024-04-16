7b60fb64040e1c8870fda4dfcc406b5eefb97416
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:09:37 +0530

    Update Q2 Pyramid.py

--- Q2 Pyramid.py
+++ Q2 Pyramid.py
@@ -1,6 +1,7 @@
 #Create a pyramid of numbers from 1 to 20 using FOR loop.
-
-for x in range(20): #Using FOR loop with the given range of values 20
-    for y in range(x+1): # Nested FOR loop to iterate the pyramid shape for each line using the 'end' method.
+#Using FOR loop with the given range of values 20
+for x in range(20): 
+    for y in range(x+1): 
+        # Nested FOR loop to iterate the pyramid shape for each line using the 'end' method.
         print(y + 1, end=" ")
     print()
